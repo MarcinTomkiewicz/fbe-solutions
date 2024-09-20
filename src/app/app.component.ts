29e9b1cd9afcee60b8b625350176cdf4c11e0c38
MarcinTomkiewicz <marc.tomk@gmail.com>
2024-09-20 07:49:43 +0200

    Components set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbScrollSpyModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FooterComponent,
+    MainComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    ContactComponent,
+    NgbScrollSpyModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'fbe-solutions';
+  title = 'FBE Solutions';
+
+  activeSection: string = 'main';
+
+  // Obsługa aktywacji sekcji Scrollspy
+  onActivate(event: any) {
+    const activeLinkId = event.relatedTarget.id; // Pobieramy ID aktywnego linku
+    console.log(event);
+    
+
+    switch (activeLinkId) {
+      case 'about-link':
+        this.activeSection = 'about';
+        break;
+      case 'services-link':
+        this.activeSection = 'services';
+        break;
+      case 'portfolio-link':
+        this.activeSection = 'portfolio';
+        break;
+      case 'contact-link':
+        this.activeSection = 'contact';
+        break;
+      default:
+        this.activeSection = 'about'; // Sekcja domyślna, np. O mnie
+    }
+  }
 }
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+alert() {
+  console.log('Hello');
+}
+
+}
