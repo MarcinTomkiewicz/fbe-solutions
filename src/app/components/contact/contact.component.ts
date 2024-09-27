966f4c2cd7f8d57054c621587ae80a62f74f9292
MarcinTomkiewicz <marc.tomk@gmail.com>
2024-09-27 10:12:10 +0200

    Adjusted responsiveness for contact form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MessageModalComponent } from '../../common/message-modal/message-modal.component';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -11,6 +15,8 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class ContactComponent {
   contactForm: FormGroup;
+  modalService = inject(NgbModal);
+  private breakpointObserver = inject(BreakpointObserver)
   topics = [
     'Zapytanie ofertowe / Wycena',
     'Zamówienie',
@@ -26,6 +32,27 @@ export class ContactComponent {
       email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.required],
       message: ['', Validators.required],
+    });
+  }
+
+  openModal() {
+    // Używamy BreakpointObserver do sprawdzenia, czy ekran jest mniejszy niż xs
+    this.breakpointObserver.observe(Breakpoints.XSmall).subscribe(result => {
+      console.log(result);
+      
+      if (result.matches) {
+        // Otwieramy modal z klasą large-modal
+        const modalRef = this.modalService.open(MessageModalComponent, { 
+          size: 'sm',
+          centered: true,
+        });
+        
+        modalRef.componentInstance.message = this.contactForm.get('message')?.value || '';
+  
+        modalRef.componentInstance.result$.pipe(take(1)).subscribe((result: string) => {
+          this.contactForm.patchValue({ message: result });
+        });
+      }
     });
   }
 
